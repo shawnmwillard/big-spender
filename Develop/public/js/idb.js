093f816b9bff4c95c23c848210b59a5bec2c2281
Shawn Willard <shawnmwillard@gmail.com>
2022-05-18 14:23:15 -0500

    attempted to setup app functionality in idb.js

--- Develop/public/js/idb.js
+++ Develop/public/js/idb.js
@@ -1,13 +1,77 @@
-// create variable to hold db connection
 let db;
+// Connecting to "budge_it_now" DB and setting to version 1
+const request = indexedDB.open("budge_it_now", 1);
 
-// establish a connection to IndexedDB called 'big-spender' and set it to version 1
-const request = indexedDB.open("big-spender", 1);
-
-// this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
+// Ensure that the database changes when updated or non-existent
 request.onupgradeneeded = function (event) {
-  // save a reference to the database
+  // Reference to the database
   const db = event.target.result;
-  //create a object store (table) called 'new-transaction', set it to have an auto incrementing primary key of sorts
-  db.createObjectStore("new-transaction", { autoIncrement: true });
+  // Object store (table) called "new_transaction" with an auto-incrementing "primary key"
+  db.createObjectStore("new_transaction", { autoIncrement: true });
 };
+
+// What happens upon successful creation of a the db with object store
+request.onsuccess = function (event) {
+  // Save reference to the db global variable
+  db = event.target.result;
+  // Check if the app is online through browser navigator object
+  if (navigator.onLine) {
+    uploadTransaction();
+  }
+};
+
+// Handles reporting any error
+request.onerror = function (event) {
+  console.log(event.target.errorCode);
+};
+
+// Function to handle offline submits
+function saveRecord(record) {
+  // Open a new transaction with the db with read/write permissions
+  const transaction = db.transaction(["new_transaction"], "readwrite");
+  // Access object store
+  const transactionObjectStore = transaction.objectStore("new_transaction");
+  // Add record to the store
+  transactionObjectStore.add(record);
+  // Dev alert
+  alert("Transaction submitted successfully!");
+}
+
+// Function to handle uploading stored transactions
+function uploadTransaction() {
+  // Open a transaction and get all data
+  const transaction = db.transaction(["new_transaction"], "readwrite");
+  const transactionObjectStore = transaction.objectStore("new_transaction");
+  const getAll = transactionObjectStore.getAll();
+
+  // If the transaction is successful
+  getAll.onsuccess = function () {
+    // Check for data
+    if (getAll.result.length > 0) {
+      // POST request
+      fetch("/api/transaction", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // Open a transaction to clear the store
+          const transaction = db.transaction(["new_transaction"], "readwrite");
+          const transactionObjectStore =
+            transaction.objectStore("new_transaction");
+          transactionObjectStore.clear();
+          // Dev alert
+          alert("Saved transactions submitted!");
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+}
+
+window.addEventListener("online", uploadTransaction);
